64f07705d203adbd2f03deb7a0224c095ab1baf5
hongwilliam <willhong20@gmail.com>
2018-01-10 10:56:02 -0500

    added comments

--- game_setup.c
+++ game_setup.c
@@ -1,21 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
+#include "headers.h"
 
 //CODE NOT TESTED YET
-//PROB LOT OF SYNTAX ERRORS, PLZ FIX IF YOU CAN
-//If you have better/efficient ways, feel free to delete some stuff
 
-struct card{
-  int value; //3 equals 0, 4 equals 1, etc
-  int suit; //diamonds = 0, clubs = 1, etc
-  char name[256]; //"3 of Diamonds", etc
-};
+//WRITE IDEAS HERE
+/**
+(1) Each card has a ranking from 0-51, each corresponding in order of increasing value
+(2) When dealing cards, the deal function first deals randomly integers from 0-51
+(3) Another function then coverts each int into the respective card
+
+*/
 
 struct card deck[52];
 struct card hand_one[13]; //assuming 4 player game
@@ -26,42 +19,36 @@ struct card hand_four[13];
 //inputs information into a card
 //this sets up the deck in order of increasing value
 //return type void
-void initialize_card(int my_value, int my_shape){
-  int ranking = (num % 4) + shape; //ex: 3 of diamond would be card 0, 3 of club would be card 1, etc
-  deck[ranking]->value = my_value;
-  deck[ranking]->suit = my_suit;
+void initialize_card(int value, int suit){
+  int ranking = (num % 4) + suit; //ex: 3 of diamond would be card 0, 3 of club would be card 1, etc
+  deck[ranking]->value = value;
+  deck[ranking]->suit = suit;
   //work on adding name later
 }
 
-//return type void
+// return type void
 void initialize_deck(){
-  int i = 0, v = 0, s = 0;
-  while (i < 52){
-    while (s < 3){
-      initialize_card(v,s);
-      s++; }
-    v++;
-    s = 0;
-    i++;
+  int card = 0, value = 0, suit = 0;
+  while (card < 52){
+    while (suit < 3){
+        deck[card]->value =
+      initialize_card(value,suit);
+      suit++; }
+    value++;
+    suit = 0;
+    card++;
   }
 }
+
+struct card deal_card();
+
+struct card deal_hands();
 
 //pseudo code only
 /**
 void deal_hands(){
-  int i = 0;
-  int random_int = rand() % 52; //not sure if this is right
-  while (i < 13){
-    hand_one[0] = deck[random_int];
-    //generate new random number not used before
-    hand_two[0] = deck[random_int];
-    //generate new random number not used before
-    hand_three[0] = deck[random_int];
-    //generate new random number not used before
-    hand_four[0] = deck[random_int];
-    i++;
-}
-
+  set up array of ints from 0-51
+  deal each only once and randomly to each of the hands
 }
 */
 
