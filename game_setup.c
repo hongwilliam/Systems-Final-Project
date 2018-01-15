52c107b9d90e7e8d7c3c07374785b3a2875a4552
hongwilliam <willhong20@gmail.com>
2018-01-15 11:21:01 -0500

    Updated testing conventions and got display card function to work

--- game_setup.c
+++ game_setup.c
@@ -1,48 +1,73 @@
 #include "headers.h"
 
-//CODE NOT TESTED YET
+//no 0 indexing for value and suit system because they make calculating card values more confusing while writing code
+//value system is 3 as 1, 4 as 2 ... A as 12, 2 as 13
+//suit system is diamonds as 1, clubs as 2, ...
 
 //WRITE IDEAS HERE
 /**
-(1) Each card has a ranking from 0-51, each corresponding in order of increasing value
-(2) When dealing cards, the deal function first deals randomly integers from 0-51
-(3) Another function then coverts each int into the respective card
-
+(1) Each card has a ranking from 1-52, each corresponding in order of increasing value (no 0 indexing to make calculations easier)
+(2) When dealing cards, the deal function first deals randomly integers from 1-52
+(3) Another function then converts each int into the respective card
 */
 
-struct card deck[52];
-struct card hand_one[13]; //assuming 4 player game
-struct card hand_two[13];
-struct card hand_three[13];
-struct card hand_four[13];
 
 //inputs information into a card
 //this sets up the deck in order of increasing value
 //return type void
 void initialize_card(int value, int suit){
-  int ranking = (num % 4) + suit; //ex: 3 of diamond would be card 0, 3 of club would be card 1, etc
-  deck[ranking]->value = value;
-  deck[ranking]->suit = suit;
-  //work on adding name later
+  int ranking = (4 * (value - 1)) + suit; //ex: 3 of diamond would be card 1, 3 of club would be card 2, etc
+  deck[ranking].value = value;
+  deck[ranking].suit = suit;
+  //ex: 5 of clubs (value: 3, suit: 2) -> ranking is (4 * (3-1)) + 2 = 10
+  //work on adding name later for display only
 }
 
 // return type void
 void initialize_deck(){
-  int card = 0, value = 0, suit = 0;
+  int card = 1, value = 1, suit = 1;
   while (card < 52){
-    while (suit < 3){
-        deck[card]->value =
-      initialize_card(value,suit);
-      suit++; }
+    while (suit < 4){
+        initialize_card(value, suit);
+        card++;
+        suit++; }
     value++;
-    suit = 0;
-    card++;
+    suit = 1;
   }
 }
 
-struct card deal_card();
+//this is ugly but necessary for easier testing
+void display_card(int ranking){
+  int card_value = deck[ranking].value;
+  int card_suit = deck[ranking].suit;
 
-struct card deal_hands();
+  char s1[16]; //value
+  char s2[16]; //suit
+  if (card_value == 1){strcpy(s1, "3 of "); }
+  if (card_value == 2){strcpy(s1, "4 of "); }
+  if (card_value == 3){strcpy(s1, "5 of "); }
+  if (card_value == 4){strcpy(s1, "6 of "); }
+  if (card_value == 5){strcpy(s1, "7 of "); }
+  if (card_value == 6){strcpy(s1, "8 of "); }
+  if (card_value == 7){strcpy(s1, "9 of "); }
+  if (card_value == 8){strcpy(s1, "10 of "); }
+  if (card_value == 9){strcpy(s1, "J of "); }
+  if (card_value == 10){strcpy(s1, "Q of "); }
+  if (card_value == 11){strcpy(s1, "K of "); }
+  if (card_value == 12){strcpy(s1, "A of "); }
+  if (card_value == 13){strcpy(s1, "2 of "); }
+
+  if (card_suit == 1){strcpy(s2, "Diamonds"); }
+  if (card_suit == 2){strcpy(s2, "Clubs"); }
+  if (card_suit == 3){strcpy(s2, "Hearts"); }
+  if (card_suit == 4){strcpy(s2, "Spades"); }
+
+  strcat(s1, s2);
+  printf("Card: %s \n", s1);
+}
+
+//struct card deal_card();
+//struct card deal_hands();
 
 //pseudo code only
 /**
@@ -54,5 +79,8 @@ void deal_hands(){
 
 int main(){
   //test later
-  return 0;
+  initialize_deck();
+  display_card(5);
+  display_card(27);
+  display_card(1);
 }
--- headers.h
+++ headers.h
@@ -10,5 +10,15 @@
 struct card{
   int value; //3 equals 0, 4 equals 1, etc
   int suit; //diamonds = 0, clubs = 1, etc
-  char name[256]; //"3 of Diamonds", etc
+  //char name[256]; //"3 of Diamonds", etc
 };
+
+struct card deck[52];
+struct card hand_one[13];
+struct card hand_two[13];
+struct card hand_three[13];
+struct card hand_four[13];
+
+void initialize_card(int value, int suit);
+void initialize_deck();
+void dislay_card(int ranking);
