55d65540aafa9b8c096c42b528c676500a2e4543
hongwilliam <willhong20@gmail.com>
2018-01-17 11:46:51 -0500

    added testing to see if combos played are valid. need to add compare combos later

--- combos.c
+++ combos.c
@@ -1,7 +1,8 @@
 #include "headers.h"
 
-//WE WORK WITH CARD RANKINGS FROM 1-52
-//there should be "choose cards in hand" function
+//WORK WITH CARD RANKINGS FROM 1-52
+//to add later: there should be "choose cards in hand" function
+//to add later: have one big "compare combo" function that combines all the 5 below
 
 //return -1 if card A < card B, 1 if card A > card B
 int compare_single(struct card A, struct card B){
@@ -21,14 +22,14 @@ int compare_double(struct card A1, struct card A2, struct card B1, struct card B
   //compare the highest ranking single from each double
   int higher_A, higher_B;
   if (compare_single(A1, A2) == -1){
-    higher_A = A2.value; }
+    higher_A = A2.rank; }
   else{
-    higher_A = A1.value; }
+    higher_A = A1.rank; }
 
   if (compare_single(B1, B2) == -1){
-    higher_B = B2.value; }
+    higher_B = B2.rank; }
   else{
-    higher_B = B1.value; }
+    higher_B = B1.rank; }
 
   if (higher_A < higher_B){
     return -1; }
@@ -36,55 +37,50 @@ int compare_double(struct card A1, struct card A2, struct card B1, struct card B
     return 1; }
 }
 
-//have one big "compare combo" function that combines all the 5 below later
 
-//ASSUMPTION: each card inputted goes in increasing ranking !!!!
-
-
-
+//input ABCDE in increasing order
 //any 5 in a row
 //return 1 if valid, 0 if not
 int check_straight(struct card A, struct card B, struct card C, struct card D, struct card E){
-  //tie breaker for comparing straights: ranking of card with highest value
-  int AB = B.rank - A.rank, BC = C.rank - B.rank, CD = D.rank - C.rank, DE = E.rank - D.rank;
-  if ( (4 <= AB <= 7) && (4 <= BC <= 7) && (4 <= CD <= 7) && (4 <= DE <= 7) ){
+  //check if values are incrementing
+  if ( ((A.value + 1) == B.value) && ((B.value + 1) == C.value) && ((C.value + 1) == D.value) && ((D.value + 1) == E.value) ){
     return 1; }
   else{
     return 0; }
+
+  //tie breaker for comparing straights: ranking of card with highest value
 }
 
 //any 5 of a suit
 //return 1 if valid, 0 if not
 int check_flush(struct card A, struct card B, struct card C, struct card D, struct card E){
-  //tie breaker with same suit: ranking of card with highest value
-  //tie breaker with different suits: higher ranked suit played
-
   //not sure if this is right
-  if (A.suit == B.suit == C.suit == D.suit == E.suit){
+  if ( (A.suit == B.suit) && (B.suit == C.suit) && (C.suit == D.suit) && (D.suit == E.suit)){
     return 1; }
   else{
-    return 0;
-  }
+    return 0; }
 
+  //tie breaker with same suit: ranking of card with highest value
+  //tie breaker with different suits: higher ranked suit played
 }
 
+//input ABC as triple (increasing order) and DE as double (increasing order)
 //any triple + double
-//input ABC as triple and DE as double
 //return 1 if valid, 0 if not
 int check_house(struct card A, struct card B, struct card C, struct card D, struct card E){
   //check case: does a triple and double exist?
-  if ((A.value == B.value == C.value) && (D.value == E.value)){
+  if ( ((A.value == B.value) && (B.value == C.value)) && (D.value == E.value)){
     return 1; }
   else{
     return 0; }
   //tie breaker: higher value of triple duh
 }
 
+//input ABCD as quadruple (increasing order)
 //4 of a kind and any 1 random card
-//input ABCD as quadruple
 //return 1 if valid, 0 if not
 int check_bomb(struct card A, struct card B, struct card C, struct card D, struct card E){
-  if (A.value == B.value == C.value == D.value){
+  if ( (A.value == B.value) && (B.value == C.value)  && (C.value == D.value) ){
     return 1; }
   else{
     return 0; }
@@ -94,7 +90,8 @@ int check_bomb(struct card A, struct card B, struct card C, struct card D, struc
 //5 in a row with same suit
 //return 1 if valid, 0 if not
 int check_straight_flush(struct card A, struct card B, struct card C, struct card D, struct card E){
-  if (1 == check_straight(A, B, C, D, E) == check_flush(A, B, C, D, E)){
+  //if conditions for both straight and flush are met
+  if ( (1 == check_straight(A, B, C, D, E) ) && (1 == check_flush(A, B, C, D, E)) ){
     return 1; }
   else{
     return 0; }
--- game_setup.c
+++ game_setup.c
@@ -178,8 +178,104 @@ void display_hand(struct card hand[]){
     i++; }
 }
 
+//return -1 if card A < card B, 1 if card A > card B
+int compare_single(struct card A, struct card B){
+  if (A.rank < B.rank){
+    return -1; }
+  else{
+    return 1; }
+}
+
+//return -1 if A1-A2 double < B1-B2 double, 1 if A1-A2 > B1-B2, 0 if not valid input
+int compare_double(struct card A1, struct card A2, struct card B1, struct card B2){
+  //first, check if A1-A2 and B1-B2 are valid double pairs
+  if (A1.value != A2.value || B1.value != B2.value){
+    return 0; }
+
+  //then the "value" of the double is the value of its highest ranking single
+  //compare the highest ranking single from each double
+  int higher_A, higher_B;
+  if (compare_single(A1, A2) == -1){
+    higher_A = A2.rank; }
+  else{
+    higher_A = A1.rank; }
+
+  if (compare_single(B1, B2) == -1){
+    higher_B = B2.rank; }
+  else{
+    higher_B = B1.rank; }
+
+  if (higher_A < higher_B){
+    return -1; }
+  else{
+    return 1; }
+}
+
+
+//input ABCDE in increasing order
+//any 5 in a row
+//return 1 if valid, 0 if not
+int check_straight(struct card A, struct card B, struct card C, struct card D, struct card E){
+  //check if values are incrementing
+  if ( ((A.value + 1) == B.value) && ((B.value + 1) == C.value) && ((C.value + 1) == D.value) && ((D.value + 1) == E.value) ){
+    return 1; }
+  else{
+    return 0; }
+
+  //tie breaker for comparing straights: ranking of card with highest value
+}
+
+//any 5 of a suit
+//return 1 if valid, 0 if not
+int check_flush(struct card A, struct card B, struct card C, struct card D, struct card E){
+  //not sure if this is right
+  if ( (A.suit == B.suit) && (B.suit == C.suit) && (C.suit == D.suit) && (D.suit == E.suit)){
+    return 1; }
+  else{
+    return 0; }
+
+  //tie breaker with same suit: ranking of card with highest value
+  //tie breaker with different suits: higher ranked suit played
+}
+
+//input ABC as triple (increasing order) and DE as double (increasing order)
+//any triple + double
+//return 1 if valid, 0 if not
+int check_house(struct card A, struct card B, struct card C, struct card D, struct card E){
+  //check case: does a triple and double exist?
+  if ( ((A.value == B.value) && (B.value == C.value)) && (D.value == E.value)){
+    return 1; }
+  else{
+    return 0; }
+  //tie breaker: higher value of triple duh
+}
+
+//input ABCD as quadruple (increasing order)
+//4 of a kind and any 1 random card
+//return 1 if valid, 0 if not
+int check_bomb(struct card A, struct card B, struct card C, struct card D, struct card E){
+  if ( (A.value == B.value) && (B.value == C.value)  && (C.value == D.value) ){
+    return 1; }
+  else{
+    return 0; }
+  //tie breaker: higher value of quadruple duh
+}
+
+//5 in a row with same suit
+//return 1 if valid, 0 if not
+int check_straight_flush(struct card A, struct card B, struct card C, struct card D, struct card E){
+  //if conditions for both straight and flush are met
+  if ( (1 == check_straight(A, B, C, D, E) ) && (1 == check_flush(A, B, C, D, E)) ){
+    return 1; }
+  else{
+    return 0; }
+}
+
+
 int main(){
   initialize_deck();
+
+  /**
   printf("testing shuffling and dealing hands \n");
   deal_hands();
 
@@ -193,6 +289,22 @@ int main(){
   display_hand(hand_three);
 
   printf("\nHere is hand four: \n");
-  display_hand(hand_four);
+  display_hand(hand_four); */
+
+  //note: input card ranking into array index (ex: 3 of diamonds is deck[1])
+  printf("checking valid straight (1) %d \n", check_straight(deck[1], deck[6], deck[12], deck[15], deck[17]) );
+  printf("checking invalid straight (0) %d \n", check_straight(deck[1], deck[2], deck[7], deck[11], deck[17]) );
+
+  printf("checking valid flush (1) %d \n", check_flush(deck[3], deck[11], deck[15], deck[23], deck[31]) );
+  printf("checking invalid flush (0) %d \n", check_flush(deck[3], deck[4], deck[11], deck[15], deck[19]) );
+
+  printf("checking valid house (1) %d \n", check_house(deck[50], deck[51], deck[52], deck[1], deck[4]) );
+  printf("checking invalid house (0) %d \n", check_house(deck[1], deck[6], deck[12], deck[15], deck[17]) );
+
+  printf("checking valid bomb (1) %d \n", check_bomb(deck[49], deck[50], deck[51], deck[52], deck[1]) );
+  printf("checking invalid bomb (0) %d \n", check_bomb(deck[1], deck[6], deck[12], deck[15], deck[17]) );
+
+  printf("checking valid straight flush (1) %d \n", check_straight_flush(deck[1], deck[5], deck[9], deck[13], deck[17]) );
+  printf("checking invalid straight flush (0) %d \n", check_straight_flush(deck[1], deck[2], deck[7], deck[11], deck[17]) );
 
 }
